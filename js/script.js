a60b7262b9a153d96dd62c1782d4899bb2f0944d
Ryan Pablo Silva Araujo <zryanaraujoo@gmail.com>
2022-02-28 23:47:59 -0300

    estruturação + estilização parte 3 - responsível

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function navbar () {
+    const nav = document.querySelector(".item3")
+    const displayMain = document.getElementById("main") 
+    const displayFigureMain = document.getElementById("item1-img")
+    
+    if (nav.style.display == '' || nav.style.display == 'none') {
+        nav.style.display = 'block'
+        displayMain.style.display = 'none'
+        displayFigureMain.style.display = 'none'
+    } else {
+        nav.style.display = 'none'
+        displayMain.style.display = 'block'
+        displayFigureMain.style.display = 'block'
+    }
+}
