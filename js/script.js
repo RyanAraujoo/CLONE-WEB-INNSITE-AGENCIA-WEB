d98988ac653ca7874b74aa103bac41a03dca1a5b
Ryan Pablo Silva Araujo <zryanaraujoo@gmail.com>
2022-03-22 12:32:56 -0300

    update scriptjs

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ function navbar () {
     const nav = document.querySelector(".navigation-navbar-fixed.header-content ul")
     const displayMain = document.getElementById("main") 
     
-    if (nav.style.display == '' || nav.style.display == 'none') {
+    if (nav.style.display == '' || nav.style.display== 'none') {
         nav.style.display = 'block'
         displayMain.style.display = 'none'
     } else {
