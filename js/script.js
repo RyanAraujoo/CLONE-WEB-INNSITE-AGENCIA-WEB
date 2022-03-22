4cdf5a1439fc47bcc1c6b948c3a09e88ca807427
Ryan Pablo Silva Araujo <zryanaraujoo@gmail.com>
2022-03-22 11:54:22 -0300

    update script classes/css

--- js/script.js
+++ js/script.js
@@ -1,19 +1,13 @@
 function navbar () {
-    const nav = document.querySelector(".navigation-fixed-header-content")
+    const nav = document.querySelector(".navigation-navbar-fixed.header-content ul")
     const displayMain = document.getElementById("main") 
-    const displayFigureMain = document.getElementById("item1-img")
-    const displayLogo = document.querySelector(".logo-fixed")
     
     if (nav.style.display == '' || nav.style.display == 'none') {
         nav.style.display = 'block'
         displayMain.style.display = 'none'
-        displayFigureMain.style.display = 'none'
-        displayLogo.style.display = 'block'
     } else {
         nav.style.display = 'none'
         displayMain.style.display = 'block'
-        displayFigureMain.style.display = 'block'
-        displayLogo.style.display = 'none'
     }
 }
 
